accb228cd0dbfb3f80edab7ca441cfc1fa3ef9ec
Hieu Dinh <hiiamaalien@gmail.com>
2019-07-03 10:47:27 -0400

    Initial

---
+++ Website Example/Assets/SideBarButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SideBarButton : MonoBehaviour
+{
+    public Sprite DownIcon;
+    public Sprite UpIcon;
+    public bool IsFocused;
+    public List<GameObject> ExpandedObject;
+}
